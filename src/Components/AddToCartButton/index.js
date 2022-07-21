d12543c5ec84ed0a90138a5b48081827301e7637
5pamm3r <cmassarino.dev@gmail.com>
2022-07-21 16:42:08 -0300

    indent

--- src/Components/AddToCartButton/index.js
+++ src/Components/AddToCartButton/index.js
@@ -1,31 +1,33 @@
-import React from 'react'
+import React from "react";
 import iconCart from "../../assets/images/icon-cart.svg";
-import './AddToCartButton.css'
+import "./AddToCartButton.css";
 
-function AddToCartButton({ setCart, count, cart}) {
+function AddToCartButton({ setCart, count, cart }) {
   const onAddToCart = () => {
-    const productName = document.querySelector("#productName").textContent;
-    const price = document.querySelector("#price").textContent;
-    const newItem = [...cart]
-    newItem.push({
-      productName: productName,
-      price: price,
-      count: count,
-      totalPrice: price * count,
-    });
-    setCart(newItem)
+    if (count > 0) {
+      const productName = document.querySelector("#productName").textContent;
+      const price = document.querySelector("#price").textContent;
+      const newItem = [...cart];
+      newItem.push({
+        productName: productName,
+        price: price,
+        count: count,
+        totalPrice: price * count,
+      });
+      setCart(newItem);
+    }
   };
 
   return (
-    <div className='AddToCartButton'>
+    <div className="AddToCartButton">
       <button className="AddToCart" onClick={onAddToCart}>
-          <span>
-            <img src={iconCart} alt="cart" />
-          </span>
-          Add to cart
-        </button>
+        <span>
+          <img src={iconCart} alt="cart" />
+        </span>
+        Add to cart
+      </button>
     </div>
-  )
+  );
 }
 
-export default AddToCartButton
+export default AddToCartButton;
